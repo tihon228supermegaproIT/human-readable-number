da95c06e883a19ee44982bb70db132776acd5ab3
tihon228supermegaproIT <119882092+tihon228supermegaproIT@users.noreply.github.com>
2023-09-04 18:49:54 +0300

    Update postinstall.js

--- postinstall.js
+++ postinstall.js
@@ -1,13 +1,34 @@
-const semver = require('semver');
-const colors = require('colors/safe');
+function toReadable(number) {
+  // Создаем массивы со словами для цифр, десятков и сотен
+  const ones = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+  const teens = ['', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+  const tens = ['', 'ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
 
-const { engines: { node: nodeVersion }} = require('./package');
+  // Если число равно нулю, возвращаем 'zero'
+  if (number === 0) {
+    return 'zero';
+  }
 
-if (!semver.satisfies(process.version, nodeVersion)) {
-  process.emitWarning(
-    colors.red(`
-      For this task we are strictly recomend you to use node ${nodeVersion}.
-      Now you are using node ${process.version}, if you are using any of features that not supported by node ${nodeVersion}, score won't be submitted
-    `)
-  );
+  // Преобразуем число в строку и разбиваем его на разряды
+  const numString = number.toString().split('');
+
+  // Если число меньше 10, возвращаем слово из массива единиц
+  if (number < 10) {
+    return ones[number];
+  }
+
+  // Если число меньше 20, возвращаем слово из массива тинов или комбинацию из массива десятков и массива единиц
+  if (number < 20) {
+    return teens[number - 10];
+  }
+
+  // Если число меньше 100, возвращаем комбинацию из массива десятков и массива единиц
+  if (number < 100) {
+    return tens[numString[0]] + ' ' + ones[numString[1]];
+  }
+
+  // Если число меньше 1000, возвращаем комбинацию из массива сотен, массива десятков и массива единиц
+  if (number < 1000) {
+    return ones[numString[0]] + ' hundred ' + toReadable(parseInt(numString.slice(1).join('')));
+  }
 }
